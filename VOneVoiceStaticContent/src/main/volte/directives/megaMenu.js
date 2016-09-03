4af84d732539b81fe919fcbe8ddfd5a3f2520bcc
Joshua Brown <nbrown.joshua@yahoo.com>
2016-09-03 17:25:01 -0400

    megaMenuDirective HTML is present Needs function to control showing menu

---
+++ VOneVoiceStaticContent/src/main/volte/directives/megaMenu.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+angular.module("mockApp")
+.directive("megaMenu", function(){
+
+	return{
+		link:function(scope, element, attrs){
+			scope.data=scope[attrs["menuSrc"]];
+		},
+		restrict:"E",
+		replace:"true",
+		templateUrl:"megaMenu.html"
+		
+		
+	}
+}).controller("mockCtrl", function($scope){
+	$scope.menuContent=[
+	                     {label:"OneVoice", link:"#",
+							subheadings:[{label:"Active Call Survey", link:"#"},{label:"CTI Dialer", link:"#"},{label:"CTI Admin", link:"#"},
+							 {label:"CTI Dashboard", link:"#"},{label:"DB ANI", link:"#"},{label:"Mgmt Tool (IMT)", link:"#"},
+							 {label:"Messaging", link:"#"},{label:"Outbound", link:"#"},{label:"Vocab", link:"#"},
+							 			]
+						},
+						{label:"User Admin", link:"https://aboutyou.verizon.com", subheadings:[]},
+						{label:"Approval Queue", link:"#", subheadings:[]}
+					   ];
+	$scope.firstHeading=function(){
+		var ans=$index===0?true:false;
+		return ans;
+	};
+	$scope.putSubheadings=function(label){
+		var ans=label.subheadings.length>0?true:false;
+		return ans;
+	};
+})
