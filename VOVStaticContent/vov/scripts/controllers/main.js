b58a8121cb5d198f9e86029ade6b4048caebd649
Jeffrey Lovell <eyekno3w@hotmail.com>
2016-09-13 11:27:10 -0400

    Created Yeoman based VOV project. It needs to have the current portions added to it, which I will do momentarily.

---
+++ VOVStaticContent/vov/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name voneVoiceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the voneVoiceApp
+ */
+angular.module('voneVoiceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
